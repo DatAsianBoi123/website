4b5e27c0b5c532bc28e4542db52bb2d77f5f68d5
DatAsianBoi123 <zach.su158@gmail.com>
2023-12-16 11:56:35 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Body } from './Body';
+import { TopBar } from './TopBar';
+import { setDark, setLight } from './colors';
 
-function App() {
+export function App() {
+  const [theme, setTheme] = useState('dark');
+
+  changeTheme(theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopBar theme={theme} setTheme={newTheme => {
+        changeTheme(newTheme);
+        setTheme(newTheme);
+      }} />
+      <Body theme={theme} />
+    </>
   );
 }
 
-export default App;
+function changeTheme(theme) {
+  if (theme === 'dark') setDark();
+  else if (theme === 'light') setLight();
+}
+
